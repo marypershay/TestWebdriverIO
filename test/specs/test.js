f5ee8f1f955d4722f1d0ac9be267edc7d15ab887
Maryia Piarshai <maryia_piarshai@epam.com>
2016-09-22 14:18:01 +0300

    update test.js

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,21 +1,18 @@
 
-describe('Onliner search', function() {
+describe('Onliner', function() {
     it('searches for Onliner', function() {
         browser.url('https://www.onliner.by/');
 		var title = browser.getTitle();
 		expect(title).toBe('Onliner.by');
-        browser.setValue('.fast-search__input', '6s 16gb Silver');
-		expect(browser.getValue('.fast-search__input')).toBe('6s 16gb Silver');
+		browser.click('=Мобильные телефоны');
+
+		browser.click('span.schema-filter__checkbox-text=Apple');
+		//browser.waitForExist('=Apple iPhone 6s 16GB Silver');
+		browser.waitForVisible('=Apple iPhone 6s 16GB Silver');
 		
-	//browser.getText('a=Apple iPhone 6s 16GB Silver').then(function() {
-   // browser.click('a=Apple iPhone 6s 16GB Silver');
-//});
-        //browser.click('.fast-search__submit');
-		//browser.click('a=Apple iPhone 6s 16GB Silver');
-		//browser.driver.switchTo().frame('modal-iframe');
-		//browser.click('a=Apple iPhone 6s 16GB Silver');
-        
-        // outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+       // browser.setValue('.fast-search__input', '6s 16gb Silver');
+	   //expect(browser.getValue('.fast-search__input')).toBe('6s 16gb Silver');
+		
 		
 		
 		
