d65552fb91d8f47dc107123c56202cc1a934b129
Maryia Piarshai <maryia_piarshai@epam.com>
2016-09-23 17:11:31 +0300

    update test.js, add chai

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,22 +1,77 @@
 
 describe('Onliner', function() {
-    it('searches for Onliner', function() {
-        browser.url('https://www.onliner.by/');
-		var title = browser.getTitle();
-		expect(title).toBe('Onliner.by');
+
+	beforeAll(function () {
+		browser.url('https://www.onliner.by/');
+
+	});
+
+
+	it('searches for Onliner', function() {
+		// var title = browser.getTitle();
+		// expect(title).toBe('Onliner.by');
+		var mobilePhones =browser.element('span.project-navigation__sign=Мобильные телефоны');
+		mobilePhones.waitForExist(5000);
 		browser.click('span.project-navigation__sign=Мобильные телефоны');
 
+	});
+
+	it('Количество точек матрицы', function() {
+		
+		var logout = browser.element('span.schema-filter__checkbox-text=Apple');
+		logout.waitForExist();
 		browser.click('span.schema-filter__checkbox-text=Apple');
-		//browser.waitForExist('=Apple iPhone 6s 16GB Silver');
-		//browser.waitForVisible('=Apple iPhone 6s 16GB Silver');
-		browser.waitForExist(500);
-		browser.click('span=Apple iPhone 6s 16GB Silver');
+
 		
-       // browser.setValue('.fast-search__input', '6s 16gb Silver');
-	   //expect(browser.getValue('.fast-search__input')).toBe('6s 16gb Silver');
-		
-		
-		
-		
-    });
+	});
+
+	it('Процессор', function() {
+		var phone = browser.element('span=Apple iPhone 6s 16GB Silver');
+		phone.waitForExist();
+		browser.click('div.schema-product__title=Apple iPhone 6s 16GB Silver');
+		var prosessor = browser.getText('span.value__text=12 Мп');
+		expect(prosessor).toBe('12 Мп');
+	});
+
+	it('Сканер отпечатка пальца ', function() {
+
+	});
+
+	it('Длина', function() {
+
+	});
+
+	it('FM-приёмник', function() {
+
+	});
 });
+
+
+
+// describe('Onliner', function() {
+
+
+
+
+// 	beforeAll(function (done) {
+// 		browser
+// 		.url('https://www.onliner.by/');
+// 			it('searches for Onliner', function() {
+// //		browser.url('https://www.onliner.by/');
+// var title = browser.getTitle();
+// expect(title).toBe('Onliner.by');
+// var mobilePhones =browser.element('span.project-navigation__sign=Мобильные телефоны');
+// mobilePhones.waitForExist(5000);
+// browser.click('span.project-navigation__sign=Мобильные телефоны');
+
+// var logout = browser.element('span.schema-filter__checkbox-text=Apple');
+// logout.waitForExist(5000);
+// browser.click('span.schema-filter__checkbox-text=Apple');
+
+// var phone = browser.element('span=Apple iPhone 6s 16GB Silver');
+// phone.waitForExist(10000);
+// browser.click('div.schema-product__title=Apple iPhone 6s 16GB Silver');
+
+// });
+// 	});
+// });
