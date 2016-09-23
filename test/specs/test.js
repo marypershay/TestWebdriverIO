96ae1e0f5252a5b349b47f767c56f0071c195d37
Maryia Piarshai <maryia_piarshai@epam.com>
2016-09-23 11:25:30 +0300

    add node_modules

--- test/specs/test.js
+++ test/specs/test.js
@@ -4,11 +4,13 @@ describe('Onliner', function() {
         browser.url('https://www.onliner.by/');
 		var title = browser.getTitle();
 		expect(title).toBe('Onliner.by');
-		browser.click('=Мобильные телефоны');
+		browser.click('span.project-navigation__sign=Мобильные телефоны');
 
 		browser.click('span.schema-filter__checkbox-text=Apple');
 		//browser.waitForExist('=Apple iPhone 6s 16GB Silver');
-		browser.waitForVisible('=Apple iPhone 6s 16GB Silver');
+		//browser.waitForVisible('=Apple iPhone 6s 16GB Silver');
+		browser.waitForExist(500);
+		browser.click('span=Apple iPhone 6s 16GB Silver');
 		
        // browser.setValue('.fast-search__input', '6s 16gb Silver');
 	   //expect(browser.getValue('.fast-search__input')).toBe('6s 16gb Silver');
